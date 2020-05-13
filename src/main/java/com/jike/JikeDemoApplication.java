33c05efedae12dc1ecb4966219ef45b3254aabce
zexin123 <919619852@qq.com>
2020-05-13 10:17:02 +0800

    极客时间100常见业务处理

---
+++ src/main/java/com/jike/JikeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JikeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JikeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jike/JikeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JikeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
